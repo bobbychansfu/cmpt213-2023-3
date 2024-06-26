28b64089cb5af6fffa121caffdb3f9757650b4ec
bobbychansfu <bobbychansfu@gmail.com>
2024-06-26 09:57:03 -0700

    poly notes

--- 5/topic5/src/main/java/ca/sfu/cmpt213/abstr/CmptStudentDemo.java
+++ 5/topic5/src/main/java/ca/sfu/cmpt213/abstr/CmptStudentDemo.java
@@ -4,6 +4,7 @@ public class CmptStudentDemo
 {
     public static void main(String[] args)
     {
+        Student s1 = new CmptStudent("john","20001",1999); // legal
         // Create a CmptStudent object.
         CmptStudent s = new CmptStudent("bob","20003",1999);
         // Display the student's data.
